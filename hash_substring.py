f8bf68c69820ff6bfb3f3d0ab1d590c05f4a2c98
EmilsK221RDB241 <125762817+EmilsK221RDB241@users.noreply.github.com>
2023-04-16 18:26:19 +0300

    Update hash_substring.py

--- hash_substring.py
+++ hash_substring.py
@@ -4,26 +4,26 @@ def read_input():
     mode = input().strip()
 
     if mode not in ['F', 'I']:
+        
         raise ValueError("Invalid input")
 
     if mode == "I":
+        
+        pattern_in = input().strip()
+        text_in = input().strip()
 
-        pattern_in = input()
-        text_in = input()
-
-        pattern = pattern_in().strip()
-        text = text_in().strip()
+        pattern = pattern_in
+        text = text_in
 
     else:
         try:
-
             with open("./tests/06") as f:
-
+                
                 pattern = f.readline().strip()
                 text = f.readline().strip()
 
         except FileNotFoundError:
-                         
+            
             print("File doesn't exist")
             exit()
 
@@ -36,7 +36,7 @@ def print_occurrences(output):
     
     
 def get_occurrences(pattern, text):
-
+    
     pattern_l = len(pattern)
     text_l = len(text)
 
@@ -46,8 +46,8 @@ def get_occurrences(pattern, text):
     x = []
 
     for n in range(text_l - pattern_l + 1):
-
-        if pattern_h == text_h and pattern == text[n: n + pattern_l]:
+        
+        if text_h == pattern_h and pattern == text[n: n + pattern_l]:
             x.append(n)
 
         if n < text_l - pattern_l:
@@ -59,4 +59,3 @@ def get_occurrences(pattern, text):
 if __name__ == '__main__':
     
     print_occurrences(get_occurrences(*read_input()))
-
